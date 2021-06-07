cf158745abeb0dbecb8be37de3c3414dedb1a37d
RazTutu <raztutu123@gmail.com>
2021-06-07 22:22:58 +0300

    Add homepage design, footer and homepage is responsive

--- bitservice/src/Components/Footer/Footer.js
+++ bitservice/src/Components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Footer.css';
+import logo_image from '../../Images/bitdefender_brand.jpg';
 
 const footer = props => {
     return(
         <React.Fragment>
-            <div className="main-footer">
-                <h2 className="main-footer__copyright">Copyright rezervat de Bitdefender</h2>
+            <div className="footer">
+                <div className="footer__logo">
+                    <img src={logo_image} alt="Bitdefender brand" class="footer__logo-image"/>
+                </div>
+                <div className="footer__info">
+                    <h1>Bitdefender</h1>
+                </div>
             </div>
         </React.Fragment>
     );
--- bitservice/src/Pages/Home.js
+++ bitservice/src/Pages/Home.js
@@ -1,11 +1,70 @@
 import React, {Component} from 'react';
 import '../CSS/Home.css';
+import add_image from '../Images/Plus_symbol.svg';
+import delete_image from '../Images/delete.jpg';
+import getIP_image from '../Images/info_no_backgr.png';
+import update_image from '../Images/refresh.jpg';
+
+
 
 class HomePage extends Component {
     render(){
         return(
             <div className="main-page">
-                <h1>Text</h1>
+                <div className="header_zone">
+                    <h1>IP Service</h1>
+                    <h2>An easy way to find if an IP belongs to AWZ, Azure or GCP.</h2>
+                </div>
+                <div className="operations">
+                    <div className="operations__content">
+                        <h1 className="operations__content-heading">Supported operations</h1>
+
+                        <div className="operations__content-items">
+                            <div className="operations__add">
+                                <img src={add_image} alt="Image for adding an IP" class="operations__content-img"/>
+                                <div className="operations__text">
+                                    <h2>Add IP</h2>
+                                    <p>   
+                                    Once an IP has been added, the service catalogs it so that it is
+                                    known it belongs to one of the following cloud service providers: 
+                                    Amazon Web Services, Google Cloud Platform,
+                                    Azure.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="operations__get">
+                                <img src={getIP_image} alt="Image for getting an IP" class="operations__content-img"/>
+                                <div className="operations__text">
+                                    <h2>Get IP</h2>
+                                    <p>
+                                    Querying the service with an IP will indicate if the IP
+                                    belongs to one of three cloud service providers-Amazon Web Services, 
+                                    Google Cloud Platform, Azure.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="operations__update">
+                                <img src={update_image} alt="Image for updating an IP" class="operations__content-img"/>
+                                <div className="operations__text">
+                                    <h2>Update IP</h2>
+                                    <p>      
+                                    Update details about an IP because there may be a switch from
+                                    one cloud provider to another.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="operations__delete">
+                                <img src={delete_image} alt="Image for deleting an IP" class="operations__content-img"/>
+                                <div className="operations__text">
+                                    <h2>Delete IP</h2>
+                                    <p>      
+                                    Delete an IP from the service.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
