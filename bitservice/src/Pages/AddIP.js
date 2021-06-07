d8bb5b697e452ff13bf3101f581cb1192240fb81
RazTutu <raztutu123@gmail.com>
2021-06-08 00:18:58 +0300

    User can make a POST with IP(Cors problem)

--- bitservice/src/Pages/AddIP.js
+++ bitservice/src/Pages/AddIP.js
@@ -1,9 +1,57 @@
 import React, {Component} from 'react';
+import '../CSS/Requests.css';
 
 class AddIPpage extends Component {
+
+    constructor(props){
+        super(props);
+        this.IP = React.createRef();
+    }
+
+    sendRequest = (pageState) =>{
+        pageState.preventDefault();
+        const input_ip = this.IP.current.value;
+        console.log("input ip is", input_ip);
+
+        const requestBody = {
+            ip: `${input_ip}`
+        }
+
+        fetch('https://boiling-reef-38533.herokuapp.com/api/v1/categorize', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if(res.status !== 200){
+                throw new Error("POST failed at fetch. The response code is not 200");
+            }
+            return res.json();
+        }).then(responseData => {
+           console.log(responseData);
+        }).catch(err => {
+            console.log(err);
+        });
+
+    }
+
     render(){
         return(
-            <h1>Add IP page</h1>
+            <React.Fragment>
+                <div className="sendRequest_container">
+                <form className="sendRequest" onSubmit={this.sendRequest}>
+                    <h2>Add an IP</h2>
+                    <div className="sendRequest__element">
+                        <label htmlFor="sendRequest__element-IP">IP:</label>
+                        <input type="text" id="sendRequest__element-IP" ref={this.IP}></input>
+                    </div>
+                    <div className="sendRequest__button">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+                </div>
+            </React.Fragment>
         );
     }
 }
