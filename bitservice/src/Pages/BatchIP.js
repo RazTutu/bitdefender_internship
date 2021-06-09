9d33b873e6bbd8c43fee8f300f6531a5d2dfeb8c
RazTutu <raztutu123@gmail.com>
2021-06-09 17:53:34 +0300

    User can batch a lot of IPs with one button pressing

--- bitservice/src/Pages/BatchIP.js
+++ bitservice/src/Pages/BatchIP.js
@@ -6,7 +6,12 @@ class BatchIPpage extends Component {
     state = {
         ip_array: [],
         arrayEmpty: true,
-        providersPressed: false
+        providersPressed: false,
+        amazon_ip_array: [],
+        azure_ip_array: [],
+        google_ip_array: [],
+        unknown_ip_array: [],
+        arrays_loaded: false
     }
 
     constructor(props){
@@ -21,7 +26,6 @@ class BatchIPpage extends Component {
 
         const ip_element = this.IP_element.current.value;
       
-        console.log("form pressed");
 
         let newStateArray = this.state.ip_array.slice();
         newStateArray.push(ip_element);
@@ -31,18 +35,54 @@ class BatchIPpage extends Component {
 
     getProviders = (pageState) => {
         pageState.preventDefault();
-        console.log("getProviders pressed");
 
         this.state.ip_array.forEach(element => {
 
             let endpoint = 'https://boiling-reef-38533.herokuapp.com/api/v1/categorize?ip=' + element;
-            console.log("endpoints is", endpoint);
+            
+            fetch(endpoint, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+                }).then(res => {
+                if(res.status !== 200){
+                    throw new Error("POST failed at fetch. The response code is not 200");
+                }
+                    return res.json();
+                }).then(responseData => {
+
+                    if(responseData.data.company === 'Azure'){
+                        let newStateAzure = this.state.azure_ip_array.slice();
+                        newStateAzure.push(element);
+                        this.setState({azure_ip_array: newStateAzure});
+
+
+                    } else if(responseData.data.company === 'Amazon Web Services'){
+                        let newStateAmazon = this.state.amazon_ip_array.slice();
+                        newStateAmazon.push(element);
+                        this.setState({amazon_ip_array: newStateAmazon});
+
+                    } else if(responseData.data.company === 'Google Cloud Platform'){
+                        let newStateGoogle = this.state.google_ip_array.slice();
+                        newStateGoogle.push(element);
+                        this.setState({google_ip_array: newStateGoogle});
+
+
+                    } else if(responseData.data.company === 'UNKNOWN'){
+                        let newStateUnknown = this.state.unknown_ip_array.slice();
+                        newStateUnknown.push(element);
+                        this.setState({unknown_ip_array: newStateUnknown});
+                    }
+
+                }).catch(err => {
+                    console.log(err);
+                });
 
         });
 
-
-
         this.setState({ip_array: []});
+        this.setState({arrays_loaded: true});
     }
 
    showList = props => {
@@ -53,6 +93,46 @@ class BatchIPpage extends Component {
             ))
             }  
         </ul>;
+   }
+
+   showGoogleList = props => {
+    return <ul>
+        {
+            this.state.google_ip_array.map((element) => (
+                <li key={element}>{element}</li>
+            ))
+        }  
+    </ul>;
+   }
+
+   showAzureList = props => {
+    return <ul>
+        {
+            this.state.azure_ip_array.map((element) => (
+                <li key={element}>{element}</li>
+            ))
+        }  
+    </ul>;
+   }
+
+   showAmazonList = props => {
+    return <ul>
+        {
+            this.state.amazon_ip_array.map((element) => (
+                <li key={element}>{element}</li>
+            ))
+        }  
+    </ul>;
+   }
+
+   showUnknownList = props => {
+    return <ul>
+        {
+            this.state.unknown_ip_array.map((element) => (
+                <li key={element}>{element}</li>
+            ))
+        }  
+    </ul>;
    }
 
 
@@ -87,6 +167,60 @@ class BatchIPpage extends Component {
                     </div>
                 </form>
 
+                <div className="show_providers">
+                    <h1>Providers</h1>
+                    <div className="show_providers__google">
+                        {this.state.arrays_loaded
+                        ?
+                        <React.Fragment>
+                            <h1>Google IPs</h1>
+                            <this.showGoogleList />
+                        </React.Fragment>
+                        :
+                        <h1>Google IPs to be loaded here</h1>
+                        }
+
+
+                    </div>
+                    <div className="show_providers__azure">
+                        {
+                           this.state.arrays_loaded
+                           ?
+                           <React.Fragment>
+                               <h1>Azure IPs</h1>
+                               <this.showAzureList />
+                           </React.Fragment> 
+                           :
+                           <h1>Azure IPs to be loaded here</h1>
+                        }
+                    </div>
+                    <div className="show_providers__amazon">
+                        {
+                            this.state.arrays_loaded
+                            ?
+                            <React.Fragment>
+                                <h1>Amazon IPs</h1>
+                                <this.showAmazonList />
+                            </React.Fragment>
+                            :
+                            <h1>Amazon IPs to be loaded here</h1>
+                        }
+                        
+                    </div>
+                    <div className="show_providers__unknown">
+                        {
+                            this.state.arrays_loaded
+                            ?
+                            <React.Fragment>
+                                <h1>Unknown IPs</h1>
+                                <this.showUnknownList />
+                            </React.Fragment>
+                            :
+                            <h1>Unknown IPs to be loaded here</h1>
+                        }
+                    </div>
+
+                </div>
 
             </React.Fragment>
         );
